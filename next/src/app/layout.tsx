5ef7e16ee37eabcb30a4feedfd1b595836f3cc9b
村瀬怜 <muraserei@Mac.lan>
2025-11-06 07:19:29 +0900

    setup: theme.tsを作成

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
