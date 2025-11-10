71b506f5762b72781574e43294fcd11f6b36efd4
村瀬怜 <muraserei@Mac.lan>
2025-11-10 15:00:45 +0900

    feat: corsの環境変数をconfigで制御する

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001", "http://127.0.0.1:3001"
+    origins Settings.front_domain
 
     resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
