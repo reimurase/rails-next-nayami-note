4f2089ecb7a6ffbeecc0c7fc2415ab271e73607e
村瀬怜 <muraserei@Mac.lan>
2025-11-11 11:26:39 +0900

    fix: corsの環境変数がnilでも落ちないようにする

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,8 +7,14 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Settings.front_domain
+    allowed_origins = [
+      "http://localhost:3000",
+      "http://127.0.0.1:3001",
+      *Array(Settings.front_domain) # nil でもOKになる
+    ].compact.uniq
 
+    origins(*allowed_origins)
+    
     resource "*",
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
