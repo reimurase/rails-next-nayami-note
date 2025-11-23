0c0f1884a1291e9a4c5d725bfcefac89f0dacd03
村瀬怜 <muraserei@Mac.lan>
2025-11-23 12:01:28 +0900

    refactor: requests/api/v1/concerns.rbをリファクタリング

--- rails/spec/requests/api/v1/concerns_spec.rb
+++ rails/spec/requests/api/v1/concerns_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::Concerns", type: :request do
   describe "POST api/v1/concerns" do
+    subject { post "/api/v1/concerns", params: params }
+
     let(:valid_params) do
       {
         concern: {
@@ -10,33 +12,34 @@ RSpec.describe "Api::V1::Concerns", type: :request do
       }
     end
 
-    it "レコードを1件作成し、201を返す" do
-      expect {
-        post "/api/v1/concerns", params: valid_params
-      }.to change { Concern.count }.by(1)
+    let(:invalid_params) do
+      {
+        concern: {
+          content: "", # バリデーションに引っかかる値
+        },
+      }
+    end
 
-      expect(response).to have_http_status(:ok)
-      json = JSON.parse(response.body)
-      expect(json["content"]).to eq "テストの悩み"
+    context "パラメータが正しいとき" do
+      let(:params) { valid_params }
+
+      it "レコードを1件作成し、201を返す" do
+        expect { subject }.to change { Concern.count }.by(1)
+
+        expect(response).to have_http_status(:ok)
+        json = JSON.parse(response.body)
+        expect(json["content"]).to eq "テストの悩み"
+      end
     end
 
     context "パラメータが不正なとき" do
-      let(:invalid_params) do
-        {
-          concern: {
-            content: "", # バリデーションに引っかかる値
-          },
-        }
-      end
+      let(:params) { invalid_params }
 
       it "レコードを作成せず、422を返す" do
-        expect {
-          post "/api/v1/concerns", params: invalid_params
-        }.not_to change { Concern.count }
+        expect { subject }.not_to change { Concern.count }
 
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
-    # subject { post(api_v1_concerns_path, headers: ) }
   end
 end
