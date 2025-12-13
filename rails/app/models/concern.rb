f7e7fa146092d6d1f632032484dc0e22db358078
村瀬怜 <muraserei@Mac.lan>
2025-12-13 10:11:13 +0900

    feat: concernのtrigger_eventの制約追加

--- rails/app/models/concern.rb
+++ rails/app/models/concern.rb
@@ -1,3 +1,4 @@
 class Concern < ApplicationRecord
   validates :content, presence: true
+  validates :trigger_event, length: { maximum: 255 }
 end
---
+++ rails/db/migrate/20251213010310_change_trigger_event_constraint_on_concerns.rb
@@ -0,0 +1,14 @@
+class ChangeTriggerEventConstraintOnConcerns < ActiveRecord::Migration[8.0]
+  def change
+    execute "UPDATE concerns SET trigger_event = '' WHERE trigger_event IS NULL"
+
+    # NULL禁止 + default 空文字
+    change_column_default :concerns, :trigger_event, ""
+    change_column_null :concerns, :trigger_event, false
+  end
+
+  def down
+    change_column_null :concerns, :trigger_event, true
+    change_column_default :concerns, :trigger_event, nil
+  end
+end
