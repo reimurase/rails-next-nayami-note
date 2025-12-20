eadec193a2cbaebae7789060b9541c32bc311ca8
村瀬怜 <muraserei@Mac.lan>
2025-12-20 11:58:59 +0900

    feat: modelのテストを追加

--- rails/app/models/concern.rb
+++ rails/app/models/concern.rb
@@ -1,4 +1,6 @@
 class Concern < ApplicationRecord
+  before_validation { self.trigger_event = "" if trigger_event.nil? }
+
   validates :content, presence: true, length: { maximum: 1000 }
   validates :trigger_event, length: { maximum: 120 }
 end
