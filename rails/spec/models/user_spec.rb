1f36e4baf65b4367c85f1386965f0f8f31880ac0
村瀬怜 <muraserei@Mac.lan>
2025-12-26 12:21:28 +0900

    feat: userモデルのバリデーションを追加

---
+++ rails/spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+# spec/models/user_spec.rb
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it "emailが必須であること" do
+      
+    end
+
+    it "emailが一意であること" do
+    end
+
+    it "passwordが必須であること（作成時）" do
+    end
+  end
+end
