9209284c3e3dfb24eddac3d702133a0ac9b09ef7
村瀬怜 <muraserei@Mac.lan>
2025-12-27 09:59:27 +0900

    feat: userモデルのバリデーションテストを追加

--- rails/spec/models/user_spec.rb
+++ rails/spec/models/user_spec.rb
@@ -3,14 +3,40 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   describe "validations" do
+    it "userが valid であること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
     it "emailが必須であること" do
-      
+      user = build(:user, email: "")
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to be_present
     end
 
     it "emailが一意であること" do
+      create(:user, email: "testA@email.com")
+      userB = build(:user, email: "testA@email.com")
+      expect(userB).not_to be_valid
+      expect(userB.errors[:email]).to be_present
     end
 
     it "passwordが必須であること（作成時）" do
+      user = build(:user, password: "")
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to be_present
+    end
+
+    it "正しいパスワードなら認証できる" do
+      user = create(:user, password: "password")
+      result = user.authenticate("password")
+      expect(result).to eq(user)
+    end
+
+    it "間違ったパスワードなら authenticate できない" do
+      user = create(:user, password: "password")
+      result = user.authenticate("wrong")
+      expect(result).to be false
     end
   end
 end
