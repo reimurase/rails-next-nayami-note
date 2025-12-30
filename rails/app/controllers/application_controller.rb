d1431b96438bad278647cefa8c545400eb58a1b0
村瀬怜 <muraserei@Mac.lan>
2025-12-30 16:06:29 +0900

    feat: login制限を追加

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   rescue_from ActiveRecord::RecordNotDestroyed, with: :render_unprocessable_entity
 
+  before_action :require_login
+
   private
 
     def render_unprocessable_entity(exception)
@@ -12,5 +14,18 @@ class ApplicationController < ActionController::API
 
     def render_not_found(_exception)
       render json: { error: "Not Found" }, status: :not_found
+    end
+
+    def current_user
+      return @current_user if defined?(@current_user)
+
+      user_id = session[:user_id]
+      @current_user = user_id && User.find_by(id: user_id)
+    end
+
+    def require_login
+      return if current_user
+
+      render json: { error: "Unauthorized" }, status: :unauthorized
     end
 end
