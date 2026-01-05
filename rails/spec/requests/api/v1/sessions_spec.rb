1b86c47cfe31732e4199ea3d8a8ca68541c331e3
村瀬怜 <muraserei@Mac.lan>
2026-01-05 15:29:10 +0900

    feat: sessionのテストを実装

--- rails/spec/requests/api/v1/sessions_spec.rb
+++ rails/spec/requests/api/v1/sessions_spec.rb
@@ -1,1 +1,20 @@
 require "rails_helper"
+RSpec.describe "Api::V1::Sessions", type: :request do
+  let(:user) { create(:user) }
+
+  it "正しい認証情報なら 200 を返す" do
+    post "/api/v1/session", params: {
+      session: { email: user.email, password: "password" },
+    }
+
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "不正な認証情報なら 401 を返す" do
+    post "/api/v1/session", params: {
+      session: { email: user.email, password: "wrong" },
+    }
+
+    expect(response).to have_http_status(:unauthorized)
+  end
+end
