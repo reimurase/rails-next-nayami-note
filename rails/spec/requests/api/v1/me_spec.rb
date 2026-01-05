0474f228dd7f2bbf828c34f8789642f54bfe903e
村瀬怜 <muraserei@Mac.lan>
2026-01-05 21:49:42 +0900

    feat: session / me でログアウトする場合のテストを実装

--- rails/spec/requests/api/v1/me_spec.rb
+++ rails/spec/requests/api/v1/me_spec.rb
@@ -3,26 +3,54 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::Me", type: :request do
   describe "GET /api/v1/me" do
-    it "未ログインの場合は 401 を返す" do
-      get "/api/v1/me"
-      expect(response).to have_http_status(:unauthorized)
+    context "未ログインの場合" do
+      it "401 を返す" do
+        get "/api/v1/me"
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
 
-    it "ログイン後は 200 を返し、 id/email を返す" do
-      user = create(:user)
+    context "ログインしている場合" do
+      it "200 を返し、 id/email を返す" do
+        user = create(:user)
 
-      post "/api/v1/session", params: {
-        session: { email: user.email, password: "password" },
-      }
-      expect(response).to have_http_status(:ok)
+        post "/api/v1/session", params: {
+          session: { email: user.email, password: "password" },
+        }
+        expect(response).to have_http_status(:ok)
 
-      get "/api/v1/me"
+        get "/api/v1/me"
 
-      expect(response).to have_http_status(:ok)
+        expect(response).to have_http_status(:ok)
 
-      json = JSON.parse(response.body)
-      expect(json["id"]).to eq(user.id)
-      expect(json["email"]).to eq(user.email)
+        json = JSON.parse(response.body)
+        expect(json["id"]).to eq(user.id)
+        expect(json["email"]).to eq(user.email)
+      end
+    end
+
+    context "ログアウトする場合" do
+      it "/me が 401 になる" do
+        user = create(:user)
+
+        # login
+        post "/api/v1/session", params: {
+          session: { email: user.email, password: "password" },
+        }
+        expect(response).to have_http_status(:ok)
+
+        # logged in確認
+        get "/api/v1/me"
+        expect(response).to have_http_status(:ok)
+
+        # logout
+        delete "/api/v1/session"
+        expect(response).to have_http_status(:no_content)
+
+        # logout後の確認
+        get "/api/v1/me"
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 end
--- rails/spec/requests/api/v1/sessions_spec.rb
+++ rails/spec/requests/api/v1/sessions_spec.rb
@@ -1,20 +1,40 @@
+# spec/requests/api/v1/sessions_spec.rb
 require "rails_helper"
 RSpec.describe "Api::V1::Sessions", type: :request do
   let(:user) { create(:user) }
+  describe "POST /api/v1/session" do
+    context "認証情報が正しい場合" do
+      it "200 を返す" do
+        post "/api/v1/session", params: {
+          session: { email: user.email, password: "password" },
+        }
 
-  it "正しい認証情報なら 200 を返す" do
-    post "/api/v1/session", params: {
-      session: { email: user.email, password: "password" },
-    }
+        expect(response).to have_http_status(:ok)
+      end
+    end
 
-    expect(response).to have_http_status(:ok)
+    context "認証情報が不正な場合" do
+      it "401 を返す" do
+        post "/api/v1/session", params: {
+          session: { email: user.email, password: "wrong" },
+        }
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
   end
 
-  it "不正な認証情報なら 401 を返す" do
-    post "/api/v1/session", params: {
-      session: { email: user.email, password: "wrong" },
-    }
+  describe "DELETE /api/v1/session" do
+    context "ログアウトする場合" do
+      it "204 を返す" do
+        post "/api/v1/session", params: {
+          session: { email: user.email, password: "password" },
+        }
+        expect(response).to have_http_status(:ok)
 
-    expect(response).to have_http_status(:unauthorized)
+        delete "/api/v1/session"
+        expect(response).to have_http_status(:no_content)
+      end
+    end
   end
 end
