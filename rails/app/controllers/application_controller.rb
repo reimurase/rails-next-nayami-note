9fd470fd8c8dda7d6528b1b931ef1fba4f54496d
村瀬怜 <muraserei@Mac.lan>
2026-01-12 13:26:38 +0900

    chore: ステータスコードをリネームして統一

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
 
     def render_unprocessable_entity(exception)
       record = exception.record
-      render json: { errors: record.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: record.errors.full_messages }, status: :unprocessable_content
     end
 
     def render_not_found(_exception)
