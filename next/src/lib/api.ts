05bda8bcd49d5646beaa51197c4173b321c10f76
村瀬怜 <muraserei@Mac.lan>
2026-01-13 09:17:11 +0900

    feat:credentialをtrueにする

--- next/src/lib/api.ts
+++ next/src/lib/api.ts
@@ -2,4 +2,7 @@ import axios from "axios";
 
 const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3000";
 
-export const api = axios.create({ baseURL });
+export const api = axios.create({
+  baseURL,
+  withCredentials: true,
+});
