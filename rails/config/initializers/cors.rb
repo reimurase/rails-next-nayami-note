2604bf65b4170d19d89dd4c669c23070b95b989a
村瀬怜 <muraserei@Mac.lan>
2026-01-16 12:26:30 +0900

    fix: csrfトークンを取得できるように修正

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -12,7 +12,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins(*allowed_origins)
 
     resource "*",
-             headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
