ef338a4ea3e44f78eb0b5aa4a4b410304e5890b8
村瀬怜 <muraserei@Mac.lan>
2026-01-17 10:45:32 +0900

    chore: フォーマット修正

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -12,8 +12,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins(*allowed_origins)
 
     resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             credentials: true
   end
 end
