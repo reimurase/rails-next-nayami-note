1e09307e989b9bfc96e3ebd9d517451468cdef23
村瀬怜 <muraserei@Mac.lan>
2026-01-20 13:12:55 +0900

    feat: authApiにlogoutを追加

--- next/src/lib/authApi.ts
+++ next/src/lib/authApi.ts
@@ -15,4 +15,6 @@ export const authApi = {
   signup: (params: SignupParams) => api.post("/api/v1/users", { user: params }),
 
   login: (params: LoginParams) => api.post("/api/v1/session", { session: params }),
+
+  logout: () => api.delete("/api/v1/session"),
 };
