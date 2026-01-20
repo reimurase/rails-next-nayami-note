f54d29174b173907396ffee30de95d7bd6fa658c
村瀬怜 <muraserei@Mac.lan>
2026-01-20 12:22:33 +0900

    feat: authApiにmeを追加

--- next/src/lib/authApi.ts
+++ next/src/lib/authApi.ts
@@ -15,6 +15,7 @@ export const authApi = {
   signup: (params: SignupParams) => api.post("/api/v1/users", { user: params }),
 
   login: (params: LoginParams) => api.post("/api/v1/session", { session: params }),
-
   logout: () => api.delete("/api/v1/session"),
+
+  me: () => api.get("/api/v1/me"),
 };
