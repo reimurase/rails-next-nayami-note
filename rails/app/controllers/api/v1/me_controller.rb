fbe8c72fc2ed360506fc25452316cc5d0dcf04c3
村瀬怜 <muraserei@Mac.lan>
2026-01-21 14:43:40 +0900

    feat: meにCache-Controlヘッダーを追加

--- rails/app/controllers/api/v1/me_controller.rb
+++ rails/app/controllers/api/v1/me_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::MeController < ApplicationController
   def show
+    response.headers["Cache-Control"] = "no-store"
     render json: {
       id: current_user.id,
       email: current_user.email,
