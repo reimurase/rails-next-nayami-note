b9c44990b582728168cef631976888402d0562cc
村瀬怜 <muraserei@Mac.lan>
2026-02-07 21:48:05 +0900

    feat: emailの最大文字数を255とするバリデーションを設定

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
 
   has_many :concerns, dependent: :destroy
 end
