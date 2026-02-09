1eae2666527e51625af64fe2fe4e31a3388c527a
村瀬怜 <muraserei@Mac.lan>
2026-02-09 12:15:40 +0900

    feat:emailのバリデーションにフォーマットの制限を追加

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :email,
+    format: { with: URI::MailTo::EMAIL_REGEXP },
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 255 } 
 
   has_many :concerns, dependent: :destroy
 end
