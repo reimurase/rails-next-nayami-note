a8777fd7891343c65a33ce9215beeb40304c8938
村瀬怜 <muraserei@Mac.lan>
2026-02-20 09:49:11 +0900

    feat: issueのDB／マイグレーション/モデルを作成

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_08_003724) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_20_003814) do
   create_table "concerns", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
@@ -18,6 +18,13 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_08_003724) do
     t.string "trigger_event", default: "", null: false, comment: "なやみのきっかけ（任意）"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_concerns_on_user_id"
+  end
+
+  create_table "issues", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
