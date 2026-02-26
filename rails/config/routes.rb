20edf7b8b5c891a2af6992c79564bf4c944cc075
村瀬怜 <muraserei@Mac.lan>
2026-02-26 11:59:45 +0900

    feat: issueのupdate系API（create/update/destroy）を追加

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       resource :session, only: [:create, :destroy]
       resources :concerns, only: [:index, :show, :create, :update, :destroy]
       resources :issues, only: [:index, :show, :create, :update, :destroy]
-      resources :roadmaps, only: [:index, :show]
+      resources :roadmaps, only: [:index, :show, :create, :update, :destroy]
     end
   end
 
