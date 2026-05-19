358c3bb2ffe87a27b8e267e65e42bdb1f1db1a6f
村瀬怜 <muraserei@Mac.lan>
2026-05-19 20:47:14 +0900

    fix: corsのヘッダー制限を厳しい設定に修正

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -12,7 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins(*allowed_origins)
 
     resource "/api/*",
-             headers: :any,
+             headers: %w[Content-Type Accept X-CSRF-Token X-Requested-With],
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
              credentials: true
   end
